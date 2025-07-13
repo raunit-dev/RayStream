781a5e947269352179aca2f7b5bbe3069854b2f3
solanagithubraunit15 <raunitjaiswal510@gmail.com>
2025-07-13 21:03:12 +0000

    txns->filtered->send->rec and process done

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,19 @@ use yellowstone_grpc_client::{GeyserGrpcClient, ClientTlsConfig};
 use dotenv::dotenv;
 use std::env;
 
-async fn setup_client() -> Result<GeyserGrpcClient<impl Interceptor>, Box<dyn std::Error>> {
+
+fn main () {
+    async fn setup_client() -> Result<GeyserGrpcClient<impl Interceptor>, Box<dyn std::Error>> {
     dotenv().ok();
     let endpoint = env::var("endpoint").expect("endpoint must be set");
     let token = env::var("token").expect("token must be set");
 
-    let client = GeyserGrpcClient::build_from_shared(endpoint.to_string())?
-        .x_token(Some(token.to_string()))?
-        .tls_config(ClientTlsConfig::new().with_native_roots())?
-        .connect()
+    let client = GeyserGrpcClient::build_from_shared(endpoint.to_string())?//takes endpoint as parameter here
+        .x_token(Some(token.to_string()))?// Set x-token
+        .tls_config(ClientTlsConfig::new().with_native_roots())?//req formation
+        .connect()//req sending
         .await?;
 
     Ok(client)
+}
 }
